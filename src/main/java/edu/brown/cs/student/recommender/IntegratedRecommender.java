b5d858f45c1af352ba8980935852f51489c45d3a
wemgnk6v <wemgnk6v@gmail.com>
2021-10-18 16:23:19 -0400

    use student class

--- src/main/java/edu/brown/cs/student/recommender/IntegratedRecommender.java
+++ src/main/java/edu/brown/cs/student/recommender/IntegratedRecommender.java
@@ -1,41 +1,39 @@
 package edu.brown.cs.student.recommender;
 
 import edu.brown.cs.student.bloomfilter.BloomFilterRecommender;
-import edu.brown.cs.student.client.User;
+import edu.brown.cs.student.client.Student;
 import edu.brown.cs.student.kdtree.KdTree;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class IntegratedRecommender<T extends Item> {
+public class IntegratedRecommender {
   // <T extends Item>
   // change User to student
-  private List<User> studentList = null;
   private BloomFilterRecommender bloomFilterRecommender = null;
   private KdTree kdTree = null;
 
-  public IntegratedRecommender(HashMap<Integer, T> studentMap) {
-    this.studentList = studentList;
+  public IntegratedRecommender(HashMap<Integer, Student> studentMap) {
     // Convert student IDs to KD tree coordinates and Bloom filter coordinates
     this.bloomFilterRecommender = new BloomFilterRecommender(studentMap, 0.0005);
     // get number of KDtree dimensions
-    this.kdTree = new KdTree(5, studentMap.values().toArray());
+    this.kdTree = new KdTree(5, (Item[]) studentMap.values().toArray());
   }
 
-  public List<Integer> getTopKRecommendationIDs(User targetItem, int k) {
-    List<T> bloomRecs = this.bloomFilterRecommender.getTopKRecommendations(targetItem, k);
+  public List<Integer> getTopKRecommendationIDs(Item targetItem, int k) {
+    List<Item> bloomRecs = this.bloomFilterRecommender.getTopKRecommendations(targetItem, k);
     int[] kdTreeRecIDs = this.kdTree.getArrayOfKnnIds(targetItem.getKdPoint(), k);
     List<Integer> mergedRecs = this.mergeRecommendationLists(bloomRecs, kdTreeRecIDs, k);
     return mergedRecs;
   }
 
-  private List<Integer> mergeRecommendationLists(List<User> bloomFilterRecs, int[] kdTreeRecs,
+  private List<Integer> mergeRecommendationLists(List<Item> bloomFilterRecs, int[] kdTreeRecs,
                                               int k) {
     List<Integer> recList = new ArrayList<Integer>();
     // add protection for when k > # students
     for (int i = 0; i < k; i++) {
-      User bloomRec = bloomFilterRecs.get(i);
+      Item bloomRec = bloomFilterRecs.get(i);
       if (!recList.contains(bloomRec) && recList.size() < k) {
         recList.add(bloomRec.getElementID());
       }
